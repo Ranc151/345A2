43c643ef88e448079841d72726388b45afe5d62f
Ranc151 <88372969+Ranc151@users.noreply.github.com>
2022-05-30 22:39:48 +1200

    add file

---
+++ 345A2.js
@@ -0,0 +1,52 @@
+window.addEventListener('mousewheel', function(event){
+    if (event.ctrlKey === true || event.metaKey) {
+        event.preventDefault();
+    }
+},{ passive: false});
+
+window.addEventListener('DOMMouseScroll', function(event){
+    if (event.ctrlKey === true || event.metaKey) {
+        event.preventDefault();
+    }
+},{ passive: false});
+
+function search(){
+    document.onkeydown=function(e) {
+        var keyNum = window.event ? e.keyCode : e.which;
+        const search_value = document.getElementById("search").value;
+        if (keyNum == 13 & search_value != "") {
+            console.log(search_value);
+        }
+    }
+}
+const check = () =>{
+    console.log("check")
+}
+const open_register = () =>{
+     document.getElementById("register").style.display = "block";
+     document.getElementById("bg").style.display = "block"
+}
+const exit = () =>{
+     document.getElementById("register").style.display = "none";
+     document.getElementById("bg").style.display = "none";
+}
+const register = () =>{
+    const givenname_value = document.getElementById("givenname").value;
+    const surname_value = document.getElementById("surname").value;
+    const username_value = document.getElementById("username").value;
+    const password_value = document.getElementById("password").value;
+    const homeaddress_value = document.getElementById("homeaddress").value;
+    const workaddress_value = document.getElementById("workaddress").value;
+    const mobilenumber_value = document.getElementById("mobilenumber").value;
+    const email_value = document.getElementById("email1").value;
+
+    if (givenname_value == "" | surname_value == "" | password_value == "" | username_value == "" | homeaddress_value =="" | workaddress_value == "" | mobilenumber_value == "" | email_value == ""){
+        alert("Please entry the user detail")
+    }
+    else{
+        alert("Register Successful")
+        document.getElementById("register").style.display = "none";
+    }
+
+}
+
